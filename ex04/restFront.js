049ce460b734daf067130ac741168cee0bf9be7b
SONSAZANG <kekekee257@gmail.com>
2022-06-10 10:48:19 +0900

    0610_4장끝_Page224

---
+++ ex04/restFront.js
@@ -0,0 +1,79 @@
+function getUser() {
+    var xhr = new XMLHttpRequest(); // 서버와 상호작용할 때 사용.
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var users = JSON.parse(xhr.responseText); // 서버에 요청하여 응답으로 받은 데이터를 문자열로 반환합니다.
+            var list = document.getElementById('list');
+            list.innerHTML = '';
+            Object.keys(users).map(function (key) {
+                var userDiv = document.createElement('div');
+                var span = document.createElement('span');
+                span.textContent = users[key];
+                var edit = document.createElement('button');
+                edit.textContent = '수정';
+                edit.addEventListener('click', function () {
+                    var name = prompt('바꿀 이름을 입력하세요');
+                    if (!name) {
+                        return alert('이름을 반드시 입력하셔야 합니다');
+                    }
+                    var xhr = new XMLHttpRequest();
+                    xhr.onload = function () {
+                        if (xhr.status === 200) {
+                            console.log(xhr.responseText);
+                            getUser();
+                        } else {
+                            console.error(xhr.responseText);
+                        }
+                    };
+                    xhr.open('PUT', '/users/' + key);
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.send(JSON.stringify({ name: name }));
+                });
+                var remove = document.createElement('button');
+                remove.textContent = '삭제';
+                remove.addEventListener('click', function () {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onload === function () {
+                        if (xhr.status === 200) {
+                            console.log(xhr.responseText);
+                            getUser();
+                        } else {
+                            console.error(xhr.responseText);
+                        }
+                    };
+                    xhr.open('DELETE', '/users/' + key);
+                    xhr.send();
+                });
+                userDiv.appendChild(span);
+                userDiv.appendChild(edit);
+                userDiv.appendChild(remove);
+                list.appendChild(userDiv);
+            });
+        } else {
+            console.error(xhr.responseText);
+        }
+    };
+    xhr.open('GET', '/users');
+    xhr.send();
+}
+window.onload = getUser;
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // 어떤 이벤트를 명시적으로 처리하지 않은 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작 실행 X
+    var name = e.target.username.value;
+    if (!name) {
+        return alert('이름을 입력하세요');
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            console.log(xhr.responseText);
+            getUser();
+        } else {
+            console.error(xhr.responseText);
+        }
+    };
+    xhr.open('POST', '/users');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({ name: name }));
+    e.target.username.value = '';
+});
